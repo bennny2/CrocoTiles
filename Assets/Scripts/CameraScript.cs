86f096a87b4742f5abcfc57118f3e551021e6870
bennny2 <benhurley01@outlook.com>
2024-04-21 15:34:10 +0100

    added underscores and encapsulated fields in camerascript

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -5,35 +5,41 @@ public class CameraScript : MonoBehaviour
 
     // Fields
 
-    public float zoomSpeed = 0.1f;
-    public float minZoomSize = 1.5f;
-    public float maxZoomSize = 3.5f;
-    private Vector3 lastMousePosition;
-    private bool isDragging = false;
+    private float _zoomSpeed = 0.1f;
+    private float _minZoomSize = 1.5f;
+    private float _maxZoomSize = 3.5f;
+    private Vector3 _lastMousePosition;
+    private bool _isDragging = false;
+
+    public float ZoomSpeed { get => _zoomSpeed; set => _zoomSpeed = value; }
+    public float MinZoomSize { get => _minZoomSize; set => _minZoomSize = value; }
+    public float MaxZoomSize { get => _maxZoomSize; set => _maxZoomSize = value; }
+    public Vector3 LastMousePosition { get => _lastMousePosition; set => _lastMousePosition = value; }
+    public bool IsDragging { get => _isDragging; set => _isDragging = value; }
 
     // Class Methods
-    
+
     void Update() {
-        if (Input.GetAxis("Mouse ScrollWheel") > 0 && Camera.main.orthographicSize > minZoomSize) {
-            Camera.main.orthographicSize -= zoomSpeed;
+        if (Input.GetAxis("Mouse ScrollWheel") > 0 && Camera.main.orthographicSize > MinZoomSize) {
+            Camera.main.orthographicSize -= ZoomSpeed;
         }
 
-        if (Input.GetAxis("Mouse ScrollWheel") < 0 && Camera.main.orthographicSize < maxZoomSize) {
-            Camera.main.orthographicSize += zoomSpeed;
+        if (Input.GetAxis("Mouse ScrollWheel") < 0 && Camera.main.orthographicSize < MaxZoomSize) {
+            Camera.main.orthographicSize += ZoomSpeed;
         }
 
         if (Input.GetMouseButtonDown(1)) {
-            lastMousePosition = Input.mousePosition;
-            isDragging = true;
+            LastMousePosition = Input.mousePosition;
+            IsDragging = true;
         }
         else if (Input.GetMouseButtonUp(1)) {
-            isDragging = false;
+            IsDragging = false;
         }
 
-        if (isDragging) {
-            Vector3 deltaMousePosition = Input.mousePosition - lastMousePosition;
+        if (IsDragging) {
+            Vector3 deltaMousePosition = Input.mousePosition - LastMousePosition;
             transform.Translate(-deltaMousePosition * Time.deltaTime);
-            lastMousePosition = Input.mousePosition;
+            LastMousePosition = Input.mousePosition;
         }
     }
 }
